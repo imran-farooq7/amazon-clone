f2451d3a651849eb0d91a70220025090dfc6a408
unknown <you@example.com>
2020-09-09 00:16:41 +0500

    created react app

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBYXiG6zsAFoR9QkMYNVkqZSs_svYWtLSI",
+  authDomain: "clone-81a8b.firebaseapp.com",
+  databaseURL: "https://clone-81a8b.firebaseio.com",
+  projectId: "clone-81a8b",
+  storageBucket: "clone-81a8b.appspot.com",
+  messagingSenderId: "94090470526",
+  appId: "1:94090470526:web:d543a86d1f76089b68e42f",
+  measurementId: "G-6RRNTRH6T2",
+};
